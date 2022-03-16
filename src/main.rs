56bf386ea53b3a5f8f7b4a5f153754e336deb10a
Justin Pumford <jpumford@gmail.com>
2022-03-15 23:16:01 -0600

    add small vga driver

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,23 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World!";
+// static HELLO: &[u8] = b"Hello World!";
 
 // use our own start function for the linker
 // don't mangle the function name so the _main label is used in the output .so
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    // let vga_buffer = 0xb8000 as *mut u8;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    // for (i, &byte) in HELLO.iter().enumerate() {
+    //     unsafe {
+    //         *vga_buffer.offset(i as isize * 2) = byte;
+    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    //     }
+    // }
+    vga_buffer::print_stuff();
 
     loop {}
 }
